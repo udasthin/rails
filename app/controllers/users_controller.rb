5c0df0d960870e70e0439dde309ad55f36c0f48f
udagawayuuki mac <udagawayuukimac@udagawayuukinoMacBook-Pro.local>
2020-03-31 01:05:09 +0900

    rails課題2がうまくいきません

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,36 @@
 class UsersController < ApplicationController
+  # 一覧画面に対応するアクション
   def index
-    @message = "Hello World"
+    @users = User.all
+  end
+# 新規作成画面に対応するアクション
+  def new
+    @user = User.new
+  end
+# 新規登録画面に対応するアクション
+  def create
+    User.create(user_params)
+  end
+  # 編集画面に対応するアクション
+  def edit
+    @user = User.find(params[:id])
+  end
+  # 更新を行うためのアクション
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+  # 削除をするためのアクション
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+  end
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 end

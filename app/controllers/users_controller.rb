70774992498fe2750968b06f6d9280922e49d94c
udagawayuuki mac <udagawayuukimac@udagawayuukinoMacBook-Pro.local>
2020-04-01 01:40:56 +0900

    課題2resourcesを使わないを提出します

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,33 +3,36 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
 # 新規作成画面に対応するアクション
   def new
     @user = User.new
   end
+
 # 新規登録画面に対応するアクション
   def create
     User.create(user_params)
   end
+
   # 編集画面に対応するアクション
   def edit
     @user = User.find(params[:id])
   end
+
   # 更新を行うためのアクション
   def update
     user = User.find(params[:id])
     user.update(user_params)
   end
+
   # 削除をするためのアクション
   def destroy
     user = User.find(params[:id])
     user.delete
   end
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
-  end
+  
   private
+  
   def user_params
     params.require(:user).permit(:name, :age)
   end
